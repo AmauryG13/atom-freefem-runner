ca561e1ced4c9564d3f3204982c0ba91715d3b6c
Amaury Guillermin <amauryguillermin@gmail.com>
2018-12-14 14:06:03 +0100

    Default package repo

---
+++ spec/freefempp-runner-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FreefemppRunnerView from '../lib/freefempp-runner-view';
+
+describe('FreefemppRunnerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
